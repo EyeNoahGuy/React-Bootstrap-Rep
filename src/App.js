2aa2c1e0564d63b2e815d923287648668c64522b
EyeNoahGuy <103152418+EyeNoahGuy@users.noreply.github.com>
2022-09-26 21:58:50 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TopBar from './components/TopBar';
+import StoreListings from './components/StoreListings';
+import Background from './components/Background';
+
+function App(){
+  return(
+  <div className='App'>
+    <TopBar/>
+    <Background/>
+    <StoreListings/>
+  </div>
+  );
+}
+export default App;
